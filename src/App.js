de17d59c9f620c42f4ae14d46066933b089c76cf
Sara Cave <saracaveg@gmail.com>
2020-05-21 21:19:06 +1000

    fixing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class Search extends Component {
+  state = {
+    text: ''
+  }
+
+
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
+  }
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value })
+
+  render() {
+    return (
+      <div>
+        <form className='form' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Search
